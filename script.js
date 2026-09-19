fc4c92d3f88200cf385fc1b19619e575c218ba16
javiersands15 <javiersands15@gmail.com>
2026-09-18 20:31:07 -0400

    Update script.js to the v0.2

--- script.js
+++ script.js
@@ -7,25 +7,83 @@ buttons.forEach(button => {
 
         if (value === "AC") {
             display.value = "";
-        } 
+        }
+
         else if (value === "DEL") {
             display.value = display.value.slice(0, -1);
-        } 
+        }
+
         else if (value === "=") {
             try {
-                display.value = eval(
-                    display.value
-                        .replace(/×/g, "*")
-                        .replace(/÷/g, "/")
-                        .replace(/−/g, "-")
-                );
+                const expression = display.value
+                    .replace(/×/g, "*")
+                    .replace(/÷/g, "/")
+                    .replace(/−/g, "-");
+
+                display.value = eval(expression);
             } catch {
                 display.value = "Error";
             }
-        } 
-        else if (value === "%") {
-            display.value = display.value / 100;
         }
+
+        else if (value === "√") {
+            try {
+                const number = Number(display.value);
+
+                if (display.value === "" || isNaN(number)) {
+                    display.value = "Error";
+                } else if (number < 0) {
+                    display.value = "Error";
+                } else {
+                    display.value = Math.sqrt(number);
+                }
+            } catch {
+                display.value = "Error";
+            }
+        }
+
+        else if (value === "x²") {
+            try {
+                const number = Number(display.value);
+
+                if (display.value === "" || isNaN(number)) {
+                    display.value = "Error";
+                } else {
+                    display.value = number ** 2;
+                }
+            } catch {
+                display.value = "Error";
+            }
+        }
+
+        else if (value === "±") {
+            try {
+                const number = Number(display.value);
+
+                if (display.value === "" || isNaN(number)) {
+                    display.value = "Error";
+                } else {
+                    display.value = number * -1;
+                }
+            } catch {
+                display.value = "Error";
+            }
+        }
+
+        else if (value === "%") {
+            try {
+                const number = Number(display.value);
+
+                if (display.value === "" || isNaN(number)) {
+                    display.value = "Error";
+                } else {
+                    display.value = number / 100;
+                }
+            } catch {
+                display.value = "Error";
+            }
+        }
+
         else {
             display.value += value;
         }
